75dbd13b436f6efffae441d357fadf974dfc9493
Been12345 <122222822+Been12345@users.noreply.github.com>
2024-06-01 23:12:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 function buttonclicked() {
   alert("e");
-  for (let i = 0; i < 10;) {
-    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-    alert(i);
-    i++;
-  }
+  window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
 }
 button.addEventListener("click", buttonclicked);
